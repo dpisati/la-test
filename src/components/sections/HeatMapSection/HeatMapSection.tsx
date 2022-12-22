99e7470e4836bc41e4bd6bbe8e1a542f4b5df538
dpisati <43629737+dpisati@users.noreply.github.com>
2022-12-22 22:55:05 +1300

    add floating button to map section

--- src/components/sections/HeatMapSection/HeatMapSection.tsx
+++ src/components/sections/HeatMapSection/HeatMapSection.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 
 import mapBg from '../../../../public/images/map.png';
+import arrowRight from '../../../../public/icons/arrowRight.svg';
+import closeIcon from '../../../../public/icons/closeIcon.svg';
 
 export default function HeatMapSection() {
   return (
-    <div className="relative flex aspect-video w-full flex-col items-center justify-center">
+    <div className="relative h-[820px] flex aspect-video w-full flex-col items-center justify-center">
       <Image
         src={mapBg}
         alt="Sea heat map"
@@ -13,6 +15,18 @@ export default function HeatMapSection() {
         sizes="100vw"
         className="object-cover"
       />
+
+      <div className="absolute flex items-center -bottom-6 lg:right-32">
+        <button className="w-[270px] bg-pw-green text-pw-grey-light py-3 rounded-full hover:brightness-110 transition-all duration-200 relative">
+          Sign Up FREE Now
+          <span className="absolute inset-y-0 right-5 flex">
+            <Image src={arrowRight} alt="Arrow right" />
+          </span>
+        </button>
+        <button className="w-6 h-6 ml-2 bg-pw-black text-pw-grey-light rounded-full hover:bg-gray-700 transition-all duration-200 flex items-center justify-center">
+          <Image src={closeIcon} alt="X" />
+        </button>
+      </div>
     </div>
   );
 }
